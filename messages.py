5a847b5b6bd41c217833584ed3bb53d46c62d3d4
MWANZIA <shem.masoo@student.moringaschool.com>
2024-01-24 12:30:45 +0300

    resource

--- messages.py
+++ messages.py
@@ -1,10 +1,20 @@
 
 from models import Message
-from flask_restful import Resource
+from flask_restful import Resource,jsonify
 from flask import make_response
 from models import db
+from seed import messages
 
-messages = []
 class message_chat(Resource):
-    def get(self)
+    def get(self):
+        message_chat = []
+        for message in Message.query.all():
+            message_dict = messages.append(message_dict)
+
+        response = make_response(
+           jsonify(messages),
+             200
+       )
+        return response
+
             
